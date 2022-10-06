5b84ecc8214e9754cf0dffe4af1cf3e3cd9788a5
Rafael Ribeiro <rafaelfelipe.r@hotmail.com>
2022-10-06 13:37:09 -0300

    preparando deploy da aplicação, trocando BrowserRouter por HashRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import store from './redux/store';
 import App from './App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={ store }>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'),
 );
